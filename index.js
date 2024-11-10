502c99982f0e3e3dd7f4c8e10fa950fea7d8a7eb
Vinicius Ferreira Citadin <viniciuscitadin@gmail.com>
2024-11-10 11:34:17 -0300

    removido documentação swagger

--- index.js
+++ index.js
@@ -1,40 +1,6 @@
 const express = require('express');
 const app = express();
 const routes = require('./src/routes');
-const cors = require('cors');
-
-app.use(cors());
-
-const swaggerUi = require('swagger-ui-express');
-const swaggerJsDoc = require('swagger-jsdoc');
-
-const options = {
-    definition: {
-        openapi: "3.0.0",
-        info: {
-            title: 'API Node.js + MongoDB',
-            version: '1.0.0',
-            description: `API utilizando Node.js, Express.js e MongoDB
-            
-            ### TD 02
-            Disciplina: DAII 2024.02 Turma 02
-            `,
-            license: {
-                name: 'Desenvolvimento de Aplicações II',
-            },
-            contact: {
-                name: 'Vinicius Ferreira Citadin',
-            },
-        },
-        servers: [
-            {
-                url: 'http://localhost:8080/',
-                description: 'Development server',
-            },
-        ],
-    },
-    apis: ["src/routes/*.js"],
-}
 
 app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*')
@@ -44,12 +10,7 @@ app.use(function(req, res, next) {
     next();
 });
 
-const specs = swaggerJsDoc(options);
-
 app.use('/', routes);
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs)),
-
-    app.listen(8080, function () {
-        console.log('Escutando na porta 8080, link: http://localhost:8080/')
-    });
+app.listen(8080, function () {
+    console.log('Escutando na porta 8080, link: http://localhost:8080/')});
--- src/routes/professionalsRoutes.js
+++ src/routes/professionalsRoutes.js
@@ -6,77 +6,11 @@ const fs = require('fs');
 const path = require('path');
 const professionalsFilePath = path.join(__dirname, '../db/professionals.json');
 
-/**
- * @swagger
- * components:
- *   schemas:
- *     Professional:
- *       type: object
- *       required:
- *         - id
- *         - name
- *         - specialty
- *         - contact
- *         - phone_number
- *         - status
- *       properties:
- *         id:
- *           type: string
- *           description: O id é gerado automáticamente pelo cadasdro do profissional
- *         name:
- *           type: string
- *           description: Nome do profissional
- *         specialty:
- *           type: string
- *           description: Especialidade do profissional
- *         contact:
- *           type: string
- *           description: E-mail de contato do profissional
- *         phone_number:
- *           type: string
- *           description: Telefone de contato do profissional
- *         status:
- *           type: string
- *           description: Situação do profissional
- *       example:
- *         id: fa1c7b2e-e41d-4d30-b3a0-8030eebbc9bb
- *         name: Dr. Carlos Silva
- *         specialty: Pediatra
- *         contact: carlos.silva@gmail.com
- *         phone_number: 48 9876 5432
- *         status: on
- */
-
- /**
-  * @swagger
-  * tags:
-  *   name: Professionals
-  *   description: 
-  *     API de Controle de Profissionais
-  *     **Por Vinicius Ferreira Citadin**
-  */
-
 const readProfessionalsDB = () => {
     const data = fs.readFileSync(professionalsFilePath);
     return JSON.parse(data);
 };
 
-/**
- * @swagger
- * /professionals:
- *   get:
- *     summary: Retorna uma lista de todos os profissionais
- *     tags: [Professionals]
- *     responses:
- *       200:
- *         description: A lista de profissionais
- *         content:
- *           application/json:
- *             schema:
- *               type: array
- *               items:
- *                 $ref: '#/components/schemas/Professional'
- */
 // Consultar todos profissionais no banco
 router.get('/', (req, res) =>{
     console.log("getroute");
@@ -84,29 +18,6 @@ router.get('/', (req, res) =>{
     res.json(professionalsDB);
 });
 
-/**
- * @swagger
- * /professionals/id/{id}:
- *   get:
- *     summary: Retorna um profissional pelo ID
- *     tags: [Professionals]
- *     parameters:
- *       - in: path
- *         name: id
- *         schema:
- *           type: string
- *         required: true
- *         description: ID do profissional
- *     responses:
- *       200:
- *         description: Um profissional pelo ID
- *         contens:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/Professional'
- *       404:
- *         description: Profissional não encontrado
- */
 // Consultar um profissional específico por id
 router.get('/id/:id', (req, res) => {
     professionalsDB = readProfessionalsDB();
@@ -123,29 +34,6 @@ router.get('/id/:id', (req, res) => {
     res.json(professional);
 });
 
-/**
- * @swagger
- * /professionals/name/{name}:
- *   get:
- *     summary: Retorna um profissional pelo nome
- *     tags: [Professionals]
- *     parameters:
- *       - in: path
- *         name: name
- *         schema:
- *           type: string
- *         required: true
- *         description: Nome do profissional
- *     responses:
- *       200:
- *         description: Um profissional pelo nome
- *         contens:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/Professional'
- *       404:
- *         description: Profissional não encontrado
- */
 // Consultar um profissional específico por nome
 router.get('/name/:name', (req, res) => {
     professionalsDB = readProfessionalsDB();
@@ -163,28 +51,6 @@ router.get('/name/:name', (req, res) => {
     res.json(professional);
 });
 
-/**
- * @swagger
- * /professionals:
- *   post:
- *     summary: Cadastra um novo profissional
- *     tags: [Professionals]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/Professional'
- *     responses:
- *       200:
- *         description: O profissional foi criado com sucesso
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/Professional'
- *       404:
- *         description: Não foi possível cadastrar o profissional
- */
 // Cadastrar um novo profissional
 router.post('/', (req, res) => {
     
@@ -216,35 +82,6 @@ router.post('/', (req, res) => {
     return res.json(professional);
 });
 
-/**
- * @swagger
- * /professionals/{id}:
- *  put:
- *    summary: Atualiza um profissional pelo ID
- *    tags: [Professionals]
- *    parameters:
- *      - in: path
- *        name: id
- *        schema:
- *          type: string
- *        required: true
- *        description: ID do profissional
- *    requestBody:
- *      required: true
- *      content:
- *        application/json:
- *          schema:
- *            $ref: '#/components/schemas/Professional'
- *    responses:
- *      200:
- *        description: O profissional foi atualizado com sucesso
- *        content:
- *          application/json:
- *            schema:
- *              $ref: '#/components/schemas/Professional'
- *      404:
- *        description: Profissional não encontrado
- */
 // Substituir o cadastro de um profissional
 router.put('/:id', (req, res) => {
     const id = req.params.id;
@@ -290,29 +127,6 @@ router.put('/:id', (req, res) => {
     return res.json(newProfessional);
 });
 
-/**
- * @swagger
- * /professionals/{id}:
- *   delete:
- *     summary: Remove um profissional pelo ID
- *     tags: [Professionals]
- *     parameters:
- *       - in: path
- *         name: id
- *         schema:
- *           type: string
- *         required: true
- *         description: ID do profissional
- *     responses:
- *       200:
- *         description: O profissional foi removido com sucesso
- *         content:
- *          application/json:
- *            schema:
- *              $ref: '#/components/schemas/Professional'
- *       404:
- *         description: Profissional não encontrado
- */
 // Deletar o cadastro de um profissional
 router.delete('/:id', (req, res) => {
     const id = req.params.id;
@@ -333,9 +147,6 @@ router.delete('/:id', (req, res) => {
     deleted = professionalsDB.splice(idProfessional, 1);
     fs.writeFileSync(professionalsFilePath, JSON.stringify(professionalsDB, null, 2));
     res.json(deleted);
-
-
-  
 });
 
 module.exports = router;
